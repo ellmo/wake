4989d32a79602c1622461233efc00106c5e05c79
ellmo <ellmunzai@gmail.com>
2011-05-09 01:25:57 +0200

    jQuery SVG
    Included Keith Wood's "jQuery SVG" plugin (http://keith-wood.name/svg.html) and added a helper to automatically include all javascript files related to it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     javascript_include_tag(*(sources.map { |js| "coffee/#{js}" }))
   end
   
+  def include_jquery_svg
+    entries = Rails.root.join("public", "javascripts", "svg").opendir.entries.grep(/(.+?\.js)/)
+    javascript_include_tag(*entries)
+  end
+  
 end
